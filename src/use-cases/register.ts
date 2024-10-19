4d1c47eb6cc27670cd57c4762979231f257fb739
Iago Figueira <iago.f.alen@gmail.com>
2024-10-19 01:23:28 -0300

    Factory Pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
-import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { User } from '@prisma/client'
+import bcrypt from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
 	name: string
@@ -18,7 +18,7 @@ export class RegisterUseCase {
 	constructor(private usersRepository: UsersRepository) {}
 
 	async execute({ name, email, password }: RegisterUseCaseRequest ): Promise<RegisterUseCaseResponse>{
-		const password_hash = await hash(password, 6)
+		const password_hash = await bcrypt.hash(password, 6)
 	
 		const userWithSameEmail = await this.usersRepository.findByEmail(email)
 	
